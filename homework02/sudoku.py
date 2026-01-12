092f89ff701f1890d81c4ccb28df116f49d5d9ad
vasilisa-osoki <osokina.vasilisa007@mail.ru>
2026-01-13 00:19:39 +0300

    feat: реализована функция поиска пустых позиций

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -107,6 +107,11 @@ def find_empty_positions(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.Tuple[in
     >>> find_empty_positions([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']])
     (2, 0)
     """
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == '.':
+                return (row, col)
+    return None  # Если пустых позиций нет
     pass
 
 
