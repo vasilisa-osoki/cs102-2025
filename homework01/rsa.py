ce3fe710b15907a93f9d29ceee3e1d2b072d8091
vasilisa-osoki <osokina.vasilisa007@mail.ru>
2026-01-12 22:15:06 +0300

    Форматирование кода с помощью black

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -18,7 +18,7 @@ def is_prime(n: int) -> bool:
         return True
     if n % 2 == 0:
         return False
-    
+
     for i in range(3, int(n**0.5) + 1, 2):
         if n % i == 0:
             return False
@@ -45,6 +45,7 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     >>> multiplicative_inverse(7, 40)
     23
     """
+
     def extended_gcd(a: int, b: int) -> tp.Tuple[int, int, int]:
         if a == 0:
             return b, 0, 1
@@ -52,16 +53,18 @@ def multiplicative_inverse(e: int, phi: int) -> int:
         x = y1 - (b // a) * x1
         y = x1
         return g, x, y
-    
+
     g, x, _ = extended_gcd(e, phi)
-    
+
     if g != 1:
         raise ValueError("Обратного элемента не существует")
-    
+
     return x % phi
 
 
-def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
+def generate_keypair(
+    p: int, q: int
+) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
     if not (is_prime(p) and is_prime(q)):
         raise ValueError("Both numbers must be prime.")
     elif p == q:
@@ -104,7 +107,7 @@ def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
     # Unpack the key into its components
     key, n = pk
     # Generate the plaintext based on the ciphertext and key using a^b mod m
-    plain = [chr((char ** key) % n) for char in ciphertext]
+    plain = [chr((char**key) % n) for char in ciphertext]
     # Return the array of bytes as a string
     return "".join(plain)
 
