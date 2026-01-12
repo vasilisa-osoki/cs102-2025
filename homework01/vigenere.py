1f7ee38c7d313ee106151d7cf6b9b6e86b75599f
vasilisa-osoki <osokina.vasilisa007@mail.ru>
2026-01-12 20:02:14 +0300

    Реализованы функции encrypt_vigenere() и decrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,6 +1,7 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
+
     >>> encrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> encrypt_vigenere("python", "a")
@@ -9,13 +10,35 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    keyword_length = len(keyword)
+    
+    for i, char in enumerate(plaintext):
+        if char.isalpha():
+            if char.isupper():
+                alphabet_start = ord('A')
+                key_char = keyword[i % keyword_length]
+                shift = ord(key_char) - ord('A')
+                
+                shifted_char = chr((ord(char) - alphabet_start + shift) % 26 + alphabet_start)
+                ciphertext += shifted_char
+            else:
+                alphabet_start = ord('a')
+                key_char = keyword[i % keyword_length].upper()
+                shift = ord(key_char) - ord('A')
+                
+                shifted_char = chr((ord(char) - alphabet_start + shift) % 26 + alphabet_start)
+                ciphertext += shifted_char
+        else:
+            ciphertext += char
+    
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     Decrypts a ciphertext using a Vigenere cipher.
+
     >>> decrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> decrypt_vigenere("python", "a")
@@ -24,5 +47,26 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    keyword_length = len(keyword)
+    
+    for i, char in enumerate(ciphertext):
+        if char.isalpha():
+            if char.isupper():
+                alphabet_start = ord('A')
+                key_char = keyword[i % keyword_length]
+                shift = ord(key_char) - ord('A')
+                
+                shifted_char = chr((ord(char) - alphabet_start - shift) % 26 + alphabet_start)
+                plaintext += shifted_char
+            else:
+                alphabet_start = ord('a')
+                key_char = keyword[i % keyword_length].upper()
+                shift = ord(key_char) - ord('A')
+                
+                shifted_char = chr((ord(char) - alphabet_start - shift) % 26 + alphabet_start)
+                plaintext += shifted_char
+        else:
+            plaintext += char
+    
     return plaintext
