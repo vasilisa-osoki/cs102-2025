567b2f3351f9db91ef16f50a5be7d269c6aad3c3
vasilisa-osoki <osokina.vasilisa007@mail.ru>
2026-01-15 07:35:04 +0300

    FIX

--- homework04/life_gui.py
+++ homework04/life_gui.py
@@ -1,9 +1,7 @@
 import pygame
 from pygame.locals import *
-
 from life import GameOfLife
 from ui import UI
-
 
 class GUI(UI):
     def __init__(self, life: GameOfLife, cell_size: int = 10, speed: int = 10) -> None:
